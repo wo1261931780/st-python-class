19566e1ca3f12b2ece1be079cacb2e0e7a80183e
junw <45444154+wo1261931780@users.noreply.github.com>
2022-10-14 23:34:58 +0800

    day1-1unit

---
+++ 2022年10月14日demo001/demo002.py
@@ -0,0 +1,14 @@
+# python和其他语言不一样
+# 不需要使用{}来进行代码分块
+
+def main():
+    i = 5
+    j = 10
+    print("我是i" + str(i) + ",我是j" + str(j))
+
+    # if __name__ == "__main__":  # 3
+
+
+main()
+
+print("结束")
