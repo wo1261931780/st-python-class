6dce24afd2914dd40c182e6b734871721aa467fa
junw <45444154+wo1261931780@users.noreply.github.com>
2022-10-21 21:45:15 +0800

    课件习题demo

--- 2022年10月21日demo002/demo010.py
+++ 2022年10月21日demo002/demo010.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 testWord = "demo"
 testWord = int(input("请输入字符串："))  # 如果在这里输入0.01之类的浮点型数据，会出现类型转换异常
 print(testWord)  # 结果为0
@@ -8,3 +11,4 @@ testNum = 12.01
 print(type(testNum))
 testNum = int(testNum)  # <class 'float'>
 print(type(testNum))  # <class 'int'>
+
