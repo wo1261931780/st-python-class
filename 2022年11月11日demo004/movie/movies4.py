1993312cdecf4864fbcfae928dd88084c461e674
junw <45444154+wo1261931780@users.noreply.github.com>
2022-11-12 18:58:39 +0800

    movie案例-生成json文件

--- 2022年11月11日demo004/movie/movies4.py
+++ 2022年11月11日demo004/movie/movies4.py
@@ -1,17 +1,6 @@
 import json
 
 
-class Director2:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def show_me_json_info(self):
-        print("{" + "\"name\":" + "\"" + self.name + "\"},")
-
-
 class Movie:
     def __init__(self, title, genre, director, studio, year):
         self.title = title
@@ -20,26 +9,10 @@ class Movie:
         self.studio = studio
         self.year = year
 
-    def show_info(self):
+    def show_info(self):  # 简单看一下基本信息
         print("this movie 《{}》,genre -- {},director -- {},studio -- {},year -- {}".format(self.title, self.genre,
                                                                                           self.director, self.studio,
                                                                                           self.year))
-
-    def show_me_json_info(self):
-        print("{" + "\"Movie\":[")
-        print("{" + "\"title\":" + "\"" + self.title + "\"},")
-        print("{" + "\"genre\":" + "\"" + self.genre + "\"},")
-        # print("\"director\":" + "\"" + self.director + "\",")
-        print("{" + "\"Director\":[")
-        if type(self.director) == list:
-            for i in self.director:
-                i.show_me_json_info()
-        else:
-            self.director.show_me_json_info()
-        print("]},")
-        print("{" + "\"studio\":" + "\"" + self.studio + "\"},")
-        print("{" + "\"year\":" + self.year + "},")
-        print("]},")
 
 
 # 定义一个函数去读取文本数据
@@ -56,51 +29,49 @@ def main():
     movies = []
     with open("movies.txt", encoding="utf-8") as f:
         for line in f:
-            title, genre, director, studio, year = line.split(";")
-            director_temporary = director.split("Director:")[1]
+            title, genre, director, studio, year = line.split(";")  # 分割字符串
+            director_temporary = director.split("Director:")[1]  # 分割字符串，保留需要的信息
+            # 判断是否有多个导演
             if "," in director_temporary:
-                Directors = []
+                multiple_director = []
                 names = str(director_temporary).split(",")
                 for i in names:
                     director_demo = dict(Name=i)
-                    Directors.append(director_demo)
-                m = Movie(title.split("Title:")[1], genre.split(":")[1], Directors, studio.split(":")[1],
+                    multiple_director.append(director_demo)
+                m = Movie(title.split("Title:")[1], genre.split(":")[1], multiple_director, studio.split(":")[1],
                           year.split(":")[1])
             else:
-                # d = Director2(director_temporary)
-                Directors2 = dict(Name=director_temporary)
-                m = Movie(title.split("Title:")[1], genre.split(":")[1], Directors2, studio.split(":")[1],
+                single_director = dict(Name=director_temporary)
+                m = Movie(title.split("Title:")[1], genre.split(":")[1], single_director, studio.split(":")[1],
                           year.split(":")[1])
             movies.append(m)
     # 打印当前的电影列表，排序
     movies.sort(key=lambda movie: movie.title)
-    # for m in movies:
-    #     m.show_me_json_info()
-    # 添加一个固定格式的字典，然后将其转换为json格式
-    movie_list = []  # 创建一个空列表，保存所有电影的信息
+    movie_aspect = []  # 创建一个空列表，保存所有电影的信息
+    # 将对象转换为字典
     for m in movies:
-        movie_dict = dict()  # 创建一个空字典，保存单个电影的信息
-        movie_dict["title"] = m.title
-        movie_dict["genre"] = m.genre
-        movie_dict["director"] = m.director
-        movie_dict["studio"] = m.studio
-        movie_dict["year"] = m.year
-        movie_list.append(movie_dict)  # 添加到列表中
+        movie_information = dict()  # 整合单个电影信息
+        movie_information["title"] = m.title
+        movie_information["genre"] = m.genre
+        movie_information["director"] = m.director
+        movie_information["studio"] = m.studio
+        movie_information["year"] = m.year
+        movie_aspect.append(movie_information)  # 保存每一部电影
         # 将字典转换为json格式
-        movies_json = json.dumps(movie_dict, ensure_ascii=False)
-        print(movies_json)
-    # ----------------------------------------
-    movie_inspect_list = []
-    for i in movie_list:
+        # movies_json = json.dumps(movie_dict, ensure_ascii=False)
+        # print(movies_json) #测试效果
+    # --------------------------------------------------------------------------------
+    movie_list_aspect = [] # 将每个电影整合到一个列表中
+    for i in movie_aspect:
         movie_inspect = dict(Movie=i)
-        movie_inspect_list.append(movie_inspect)
-    movies_final = dict(Movies=movie_inspect_list)
-    print(movies_final)  # 打印最终的字典
-    # ----------------------------------------
+        movie_list_aspect.append(movie_inspect)
+    movies_collection = dict(Movies=movie_list_aspect)
+    print(movies_collection)  # 打印最终的字典
+    # --------------------------------------------------------------------------------
 
     with open("C:\\Users\\64234\PycharmProjects\\st-python-class.github.io\\2022年11月11日demo004\movie\\result.json",
               "w") as f:
-        json.dump(movies_final, f)
+        json.dump(movies_collection, f)
         print("加载入文件完成...")
     f.close()
 
