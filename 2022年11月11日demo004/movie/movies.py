090b791b7dfea7e00c980c95fb5748022a856058
junw <45444154+wo1261931780@users.noreply.github.com>
2022-11-12 16:21:28 +0800

    movie案例

--- 2022年11月11日demo004/movie/movies.py
+++ 2022年11月11日demo004/movie/movies.py
@@ -2,6 +2,10 @@ class Director:
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return self.name
+
+
 class Movie:
     def __init__(self, title, genre, director, studio, year):
         self.title = title
@@ -11,9 +15,9 @@ class Movie:
         self.year = year
 
     def show_info(self):
-        print("this movie {},genre is {},director is {},studio is {},year is {}".format(self.title, self.genre,
-                                                                                        self.director, self.studio,
-                                                                                        self.year))
+        print("this movie 《{}》,genre -- {},director -- {},studio -- {},year -- {}".format(self.title, self.genre,
+                                                                                          self.director, self.studio,
+                                                                                          self.year))
 
 
 # 定义一个函数去读取文本数据
@@ -26,31 +30,21 @@ class Movie:
 
 def main():
     movies = []
-    with open("movie.txt", encoding="utf-8") as f:
+    with open("movies.txt", encoding="utf-8") as f:
         for line in f:
-            title, genre, director, studio, year = line.split(",")
-            d = Director(director)
-            m = Movie(title, genre, d, studio, year)
+            print("我是读取的数据：", line)
+            title, genre, director, studio, year = line.split(";")
+            print("我是分割后的数据：", title, genre, director, studio, year)
+            d = Director(director.split(":")[1])
+            print("我是导演对象：", d.name)
+            m = Movie(title.split(":")[1], genre.split(":")[1], d, studio.split(":")[1], year.split(":")[1])
+            print("我是电影对象：", m.title, m.genre, m.director, m.studio, m.year)
             movies.append(m)
     for m in movies:
         m.show_info()
-with open("movies.txt") as f:
-    text_data = f.readlines()
-    # while not data is None: # 避免重复调用
-    print("我是读取的数据：", text_data)
-    print(type(text_data))  # list
 
-    for d in text_data:
-        print("我是当前行：", d)
-        if d == "[\n":
-            print("我是if：", d)
-            new_movie = Movie()
-            while d != "]\n":
-                print("我是while：", d)
-                new_movie.title = d
-            print("我是if结束：", d)
+    f.close()
 
-f.close()
 
 # def Iterator():
 
