cd043ec8f80287a9c3b5e5ee59b8d02906594295
junw <45444154+wo1261931780@users.noreply.github.com>
2022-11-12 17:05:36 +0800

    movie案例

--- 2022年11月11日demo004/movie/movies2.py
+++ 2022年11月11日demo004/movie/movies2.py
@@ -5,6 +5,9 @@ class Director:
     def __str__(self):
         return self.name
 
+    def show_me_json_info(self):
+        print("{" + "\"name\":" + "\"" + self.name + "\"},")
+
 
 class Movie:
     def __init__(self, title, genre, director, studio, year):
@@ -19,6 +22,18 @@ class Movie:
                                                                                           self.director, self.studio,
                                                                                           self.year))
 
+    def show_me_json_info(self):
+        print("{" + "\"Movie:[")
+        print("{" + "\"title\":" + "\"" + self.title + "\"},")
+        print("{" + "\"genre\":" + "\"" + self.genre + "\"},")
+        # print("\"director\":" + "\"" + self.director + "\",")
+        print("{" + "\"Director:[")
+        for i in self.director:
+            i.show_me_json_info()
+        print("]},")
+        print("{" + "\"studio\":" + "\"" + self.studio + "\"},")
+        print("{" + "\"year\":" + "\"" + self.year + "\"},")
+
 
 # 定义一个函数去读取文本数据
 # 定义一个电影对象,属性就是名称,类型,导演,制片厂,年份
@@ -32,10 +47,7 @@ def main():
     movies = []
     with open("movies.txt", encoding="utf-8") as f:
         for line in f:
-            print("我是读取的数据：", line)
             title, genre, director, studio, year = line.split(";")
-            print("我是分割后的数据：", title, genre, director, studio, year)
-            # d = Director(director.split("Director:")[1])
             director_temporary = director.split("Director:")[1]
             if "," in director_temporary:
                 Directors = []
@@ -43,18 +55,14 @@ def main():
                 for i in names:
                     director_demo = Director(i)
                     Directors.append(director_demo)
-                    print("我是导演对象：", director_demo.name)
                 m = Movie(title.split("Title:")[1], genre.split(":")[1], Directors, studio.split(":")[1],
                           year.split(":")[1])
-                print("我是电影对象：", m.title, m.genre, m.director, m.studio, m.year)
             else:
                 d = Director(director_temporary)
-                print("我是导演对象：", d.name)
                 m = Movie(title.split("Title:")[1], genre.split(":")[1], d, studio.split(":")[1], year.split(":")[1])
-                print("我是电影对象：", m.title, m.genre, m.director, m.studio, m.year)
             movies.append(m)
     for m in movies:
-        m.show_info()
+        m.show_me_json_info()
     f.close()
 
 
