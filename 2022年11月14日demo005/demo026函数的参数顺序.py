f20738e7eb912e32c89b64cb584517e68a02207b
junw <45444154+wo1261931780@users.noreply.github.com>
2022-12-04 13:28:49 +0800

    Update demo026函数的参数顺序.py

--- 2022年11月14日demo005/demo026函数的参数顺序.py
+++ 2022年11月14日demo005/demo026函数的参数顺序.py
@@ -3,9 +3,10 @@
 # 就会直接报错
 def print_info(num1, num2, num3=0, num4=0):
     print("num1:{},num2:{},num3:{},num4:{}".format(num1, num2, num3, num4))
-    return
+    return num1+num2+num3+num4
 
 
 if __name__ == '__main__':
     print_info(1, 0, 0, 1)
+    print(print_info(1, 2, 3))
     print_info(1, 0, num4=11) # 直接跳过一个参数，给第四个参数赋值
