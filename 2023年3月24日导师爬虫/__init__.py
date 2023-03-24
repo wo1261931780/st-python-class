e3cbc8eeb82fd5c2adc414d188f07b6fddbe2ce5
junw <45444154+wo1261931780@users.noreply.github.com>
2023-03-24 10:43:51 +0800

    1

---
+++ 2023年3月24日导师爬虫/__init__.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ma.sjtu.edu.cn/szdw/jsml.htm'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+professors = []
+
+for professor in soup.find_all('div', class_='teacher-info'):
+	name = professor.find('h3').text.strip()
+	title = professor.find('span').text.strip()
+	research_area = professor.find('p', class_='teacher-info-desc').text.strip()
+	professors.append({'name': name, 'title': title, 'research_area': research_area})
+
+print(professors)
+
+# 我需要一个完整的爬虫程序，请使用Python编写代码。然后将获得的数据保存到demo.txt文件中。爬虫地址为：https://ma.sjtu.edu.cn/szdw/jsml.htm
---
+++ 2023年3月24日导师爬虫/demo.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://ma.sjtu.edu.cn/szdw/jsml.htm'
+response = requests.get(url)
+response.encoding = response.apparent_encoding
+
+soup = BeautifulSoup(response.text, 'html.parser')
+teachers = soup.select('.teacher-list li')
+
+print(teachers)
+
+with open('demo.txt', 'w', encoding='utf-8') as f:
+	for teacher in teachers:
+		name = teacher.select_one('h3').text.strip()
+		title = teacher.select_one('.teacher-title').text.strip()
+		research = teacher.select_one('.teacher-research').text.strip()
+		f.write(f'{name}\t{title}\t{research}\n')
